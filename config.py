cc4dc11372f8ebe5ca8f38d1d66d220135ff7bbd
suryadeeprampur <suryadeep.rampur@gmail.com>
2025-08-26 09:23:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,8 +59,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
-SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "gplinks.in") # shortlink domain without https://
+SHORTLINK_API = environ.get("SHORTLINK_API", "fa7b06fadcb10ca159f29be425adcbae662469b5") # shortlink api
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
 # Website Info:
